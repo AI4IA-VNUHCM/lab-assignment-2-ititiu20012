b3c6bfba40b358fe64f1ff59a15c98f05d87c001
ititiu20012 <ititiu20012@student.hcmiu.edu.vn>
2021-10-11 13:53:09 +0700

    Ex1

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -21,7 +21,46 @@ int main(int argc, char *argv[]) {
 		test_array[i] = atoi(argv[i+1]);
 	}
 	//Your codes here
-	
+	int *a,n,i,j,temp;
+	printf("Enter size of array:");
+	scanf("%d",&n);
+
+	a=malloc(sizeof(int)*n);
+
+	printf("Enter %d Elements:",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n/2;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+
+		for(j=n/2;j<n-1;j++)
+		{
+			if(a[j]<a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+			}
+		}
+	}
+	printf("After sorting first half in ascending and second half in descending order:\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
 	
 	return 0;
 }
+
