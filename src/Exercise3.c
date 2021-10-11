3d01778a6c20cb342978f76b6dd5f9fdb43cc0e5
ititiu20012 <ititiu20012@student.hcmiu.edu.vn>
2021-10-11 17:43:11 +0700

    Update Exercise3.c

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -9,31 +9,29 @@ ______________________________________
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int i;
-void primefactors(int n)
-	{
-		while ( n % 2==0)
-		{
-			printf("%d ", 2);
-			n = n/2;
-		}
-		for (int i=3; i<= sqrt(n); i=i+2)
-		{
-			while (n%i ==0)
-			{
-				printf("%d *",i);
-				n=n/i;
-			}
-		}
-		if (n>2)
-		printf(" %d ",n);
-	}
-
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
-	int n = atoi(argv[1]);
-	//Your codes here	
-	primefactors(n);
+	int testcase = atoi(argv[1]);
+	
+	//Your codes here
+	int i=2;
+	while (testcase!=1)
+	{ 	if (testcase%i==0){
+		testcase=testcase/i;
+		printf("%d ",i);
+		if(testcase!=1){
+			printf("* ");
+		}
+		i=2;
+	}
+	    else{
+			i++;
+		}
+	
+		
+	}
+	
+	
 	return 0;
 }
